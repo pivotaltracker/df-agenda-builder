6f2e1f7d818705686df98d96c8f75e8cba911ff2
Morgan Whitney <mwhitney@pivotal.io>
2018-08-22 16:23:32 -0600

    pretty header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,46 @@
 import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import BannerWatermark from '../images/banner_watermark.png';
+import TrackerLogo from '../images/Tracker_Horizontal.svg';
 
-const Header = () => {
-  return(
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="title" color="inherit">
-          Build an Activities
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
+const styles = {
+  header: {
+    backgroundImage: `url(${BannerWatermark})`,
+    backgroundColor: '#49718F',
+    height: '100px',
+    display: 'flex',
+    alignItems: 'right',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  logo: {
+    margin: '10px 10px 0 0',
+    width: '200px',
+  }
+};
+
+
+class Header extends React.Component {
+  render() {
+    let {
+      classes,
+    } = this.props;
+
+    return(
+      <div className={classes.header}>
+        <img className={classes.logo} src={TrackerLogo} />
+      </div>
+    );
+  }
+
 }
 
-export default Header;
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
